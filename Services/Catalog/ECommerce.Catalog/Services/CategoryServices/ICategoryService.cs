03b58f3bb37468060b20319e3b47307b3a7ba269
burakgunce <brk.gnc9@hotmail.com>
2024-06-03 20:58:16 +0300

    category service was written

---
+++ Services/Catalog/ECommerce.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Catalog.DTOs.CategoryDtos;
+
+namespace ECommerce.Catalog.Services.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GettAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(string id);
+        Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id);
+    }
+}
