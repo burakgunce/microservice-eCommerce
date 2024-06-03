8d292af2932a066dc09984c7455e6ddc86750abb
burakgunce <brk.gnc9@hotmail.com>
2024-06-04 02:42:38 +0300

    controllers were added

--- Services/Catalog/ECommerce.Catalog/Program.cs
+++ Services/Catalog/ECommerce.Catalog/Program.cs
@@ -1,3 +1,11 @@
+using ECommerce.Catalog.Services.CategoryServices;
+using ECommerce.Catalog.Services.ProductDetailServices;
+using ECommerce.Catalog.Services.ProductImageServices;
+using ECommerce.Catalog.Services.ProductServices;
+using ECommerce.Catalog.Settings;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+
 namespace ECommerce.Catalog
 {
     public class Program
@@ -7,6 +15,18 @@ namespace ECommerce.Catalog
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddScoped<ICategoryService, CategoryService>();
+            builder.Services.AddScoped<IProductService, ProductService>();
+            builder.Services.AddScoped<IProductDetailService, ProductDetailService>();
+            builder.Services.AddScoped<IProductImageService, ProductImageService>();
+
+            builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+            builder.Services.AddScoped<IDatabaseSettings>(sp =>
+            {
+                return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+            });
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
