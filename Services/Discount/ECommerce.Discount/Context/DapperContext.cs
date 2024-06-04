644f3b3d3360d6b3a9fbec5046c002e3a1a14399
burakgunce <brk.gnc9@hotmail.com>
2024-06-05 00:49:14 +0300

    discount service structure was prepared

---
+++ Services/Discount/ECommerce.Discount/Context/DapperContext.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Discount.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace ECommerce.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-7G5LBG3\\SQLSERVERBG;database=ECommerceDiscountDb;integrated security=true;MultipleActiveResultSets=true");
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
