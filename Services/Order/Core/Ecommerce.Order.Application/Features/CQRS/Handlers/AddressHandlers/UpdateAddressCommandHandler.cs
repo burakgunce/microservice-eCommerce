f1e4687fbf808d860e2e54687d37c46444e84584
burakgunce <brk.gnc9@hotmail.com>
2024-06-08 21:49:10 +0300

    cqrs structure was prepared for orders address

---
+++ Services/Order/Core/Ecommerce.Order.Application/Features/CQRS/Handlers/AddressHandlers/UpdateAddressCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce.Order.Application.Features.CQRS.Commands.AddressCommands;
+using Ecommerce.Order.Application.Interfaces;
+using ECommerce.Order.Domain.Entities;
+
+namespace Ecommerce.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class UpdateAddressCommandHandler
+    {
+        private readonly IRepository<Address> _repository;
+        public UpdateAddressCommandHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(UpdateAddressCommand command)
+        {
+            var values = await _repository.GetByIdAsync(command.AddressId);
+            values.Detail1 = command.Detail;
+            values.District = command.District;
+            values.City = command.City;
+            values.UserId = command.UserId;
+            await _repository.UpdateAsync(values);
+        }
+    }
+}
