f848a01b39cfe6d8ca25df95c717efc1b0f4e967
burakgunce <brk.gnc9@hotmail.com>
2024-06-20 21:51:24 +0300

    config was updated for clients

--- IdentityServer/ECommerce.IdentityServer/Config.cs
+++ IdentityServer/ECommerce.IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -29,6 +30,45 @@ namespace ECommerce.IdentityServer
             new ApiScope("CatalogReadPermission","Reading authority for catalog operations"),
             new ApiScope("DiscountFullPermission","Full authority for discount operations"),
             new ApiScope("OrderFullPermisson","Full authority for order operations")
+        };
+
+        public static IEnumerable<Client> Clients => new Client[]
+        {
+            //Visitor
+            new Client
+            {
+                ClientId="ECommerceVisitorId",
+                ClientName="ECommerce Visitor User",
+                AllowedGrantTypes=GrantTypes.ClientCredentials,
+                ClientSecrets={new Secret("ecommercesecret".Sha256())},
+                AllowedScopes={"CatalogReadPermission"}
+            },
+
+            //Manager
+            new Client
+            {
+                ClientId="ECommerceManagerId",
+                ClientName="ECommerce Manager User",
+                AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
+                ClientSecrets={new Secret("ecommercesecret".Sha256()) },
+                AllowedScopes={ "CatalogReadPermission", "CatalogFullPermission"}
+            },
+
+            //Admin
+            new Client
+            {
+                ClientId="ECommerceAdminId",
+                ClientName="ECommerce Admin User",
+                AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
+                ClientSecrets={new Secret("ecommercesecret".Sha256()) },
+                AllowedScopes={ "CatalogFullPermission", "CatalogReadPermission", "DiscountFullPermission", "OrderFullPermisson",
+                IdentityServerConstants.LocalApi.ScopeName,
+                IdentityServerConstants.StandardScopes.Email,
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile
+                },
+                AccessTokenLifetime=600
+            }
         };
     }
 }
