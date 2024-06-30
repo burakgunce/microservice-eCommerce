f1fbc8622c7327fd8a98ac15340a805d4fbdc967
burakgunce <brk.gnc9@hotmail.com>
2024-06-30 23:09:50 +0300

    entities and context have been prepared for cargo service

---
+++ Services/Cargo/ECommerce.Cargo.DataAccess/Context/CargoContext.cs
@@ -0,0 +1,22 @@
+﻿using ECommerce.Cargo.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Cargo.DataAccess.Context
+{
+    public class CargoContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1441;initial Catalog=MultiShopCargoDb;User=sa;Password=123456aA*");
+        }
+        public DbSet<CargoCompany> CargoCompanies { get; set; }
+        public DbSet<CargoDetail> CargoDetails { get; set; }
+        public DbSet<CargoCustomer> CargoCustomers { get; set; }
+        public DbSet<CargoOperation> CargoOperations { get; set; }
+    }
+}
---
+++ Services/Cargo/ECommerce.Cargo.Entity/Concrete/CargoOperation.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.Cargo.Entity.Concrete
+{
+    public class CargoOperation
+    {
+        public int CargoOperationId { get; set; }
+        public string Barcode { get; set; }
+        public string Description { get; set; }
+        public DateTime OperationDate { get; set; }
+    }
+}
