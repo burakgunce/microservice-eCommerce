b639c158f8921326acf571368d30af57abe5b491
burakgunce <brk.gnc9@hotmail.com>
2024-07-04 18:05:38 +0300

    services and controller have been prepared for basket microservice

--- Services/Basket/ECommerce.Basket/Controllers/BasketsController.cs
+++ Services/Basket/ECommerce.Basket/Controllers/BasketsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ECommerce.Basket.DTOs;
+using ECommerce.Basket.LoginServices;
+using ECommerce.Basket.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Basket.Controllers
@@ -7,5 +10,36 @@ namespace ECommerce.Basket.Controllers
     [ApiController]
     public class BasketsController : ControllerBase
     {
+        private readonly IBasketService _basketService;
+        private readonly ILoginService _loginService;
+
+        public BasketsController(IBasketService basketService, ILoginService loginService)
+        {
+            _basketService = basketService;
+            _loginService = loginService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMyBasketDetail()
+        {
+            var user = User.Claims;
+            var values = await _basketService.GetBasket(_loginService.GetUserId);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveMyBasket(BasketTotalDto basketTotalDto)
+        {
+            basketTotalDto.UserId = _loginService.GetUserId;
+            await _basketService.SaveBasket(basketTotalDto);
+            return Ok("Sepetteki değişiklikler kaydedili");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBasket()
+        {
+            await _basketService.DeleteBasket(_loginService.GetUserId);
+            return Ok("Sepet başarıyla silindi");
+        }
     }
 }
---
+++ Services/Basket/ECommerce.Basket/Services/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿using ECommerce.Basket.DTOs;
+
+namespace ECommerce.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<BasketTotalDto> GetBasket(string userId);
+        Task SaveBasket(BasketTotalDto basketTotalDto);
+        Task DeleteBasket(string userId);
+    }
+}
