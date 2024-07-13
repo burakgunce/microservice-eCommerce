5043d5f3633a38a865629ec0fa9e6c37de8e0d0e
burakgunce <brk.gnc9@hotmail.com>
2024-07-13 23:54:39 +0300

    topbar and navbar partials were prepared

---
+++ Frontends/ECommerce.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Catalog.Services.CategoryServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _NavbarUILayoutComponentPartial : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
