1af65a091a2fe0c88e3e54df8d3a803f1b308656
burakgunce <brk.gnc9@hotmail.com>
2024-07-14 01:02:27 +0300

    special offer and fetaure components of partials were prepared

---
+++ Frontends/ECommerce.WebUI/ViewComponents/DefaultViewComponents/_SpeacialOfferComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _SpeacialOfferComponentPartial : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
