ae73bdc44b31c3f6879c70efdd22be21dfefadcf
burakgunce <brk.gnc9@hotmail.com>
2024-08-02 21:28:55 +0300

    category views were prepared

---
+++ Frontends/ECommerce.WebUI/ViewComponents/DefaultViewComponents/_CategoriesDefaultComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Catalog.Services.CategoryServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _CategoriesDefaultComponentPartial : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
