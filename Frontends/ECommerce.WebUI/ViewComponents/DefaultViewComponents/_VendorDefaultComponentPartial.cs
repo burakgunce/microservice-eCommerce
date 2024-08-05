5682b55e797f277cd3fda081344c60a2429779f9
burakgunce <brk.gnc9@hotmail.com>
2024-08-05 20:28:45 +0300

    vendor and footer part were prepared

---
+++ Frontends/ECommerce.WebUI/ViewComponents/DefaultViewComponents/_VendorDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _VendorDefaultComponentPartial : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/ECommerce.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _FooterUILayoutComponentPartial : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
