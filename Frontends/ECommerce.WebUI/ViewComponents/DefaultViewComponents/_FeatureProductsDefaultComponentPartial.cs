b3121e3edb1e5c9380d47a42fc5814c125fb5c65
burakgunce <brk.gnc9@hotmail.com>
2024-08-05 20:13:54 +0300

    features and  special offers part were prepared

---
+++ Frontends/ECommerce.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Catalog.Services.ProductServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _FeatureProductsDefaultComponentPartial : ViewComponent
+    {
+        
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
